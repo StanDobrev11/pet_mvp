76ddd57d525caf6c4a5f12aed07d14146b7cff6c
MASTER-PC <dobrev81@gmail.com>
2025-05-02 10:01:31 +0000

    fixed photo issue

--- pet_mvp/pets/utils.py
+++ pet_mvp/pets/utils.py
@@ -1,4 +1,7 @@
 def pet_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/pets/<filename>
+    if not instance.id:
+        instance.save()
+
     _, extension = filename.split('.')
     return f'pets/{instance.id}_{instance.name}.{extension}'
