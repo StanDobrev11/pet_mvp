bdf82cee707cc6a4bb46b69f1de59fec255d8a0b
MASTER-PC <dobrev81@gmail.com>
2025-05-20 19:51:05 +0000

    created task and signal

---
+++ pet_mvp/accounts/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
+from pet_mvp.notifications.tasks import send_user_registration_email
+
+UserModel = get_user_model()
+
+
+@receiver(signal=post_save, sender=UserModel)
+def user_registration_signal(sender, instance, **kwargs):
+    """Signal handler to send email notification on user registration"""
+
+    send_user_registration_email(instance)
