72cee0b183437d24e5e024b3efd5a178e054e5ca
MASTER-PC <dobrev81@gmail.com>
2025-05-24 12:21:44 +0000

    completed feature

--- pet_mvp/common/urls.py
+++ pet_mvp/common/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_not_required
 from django.urls import path, include
 
-from pet_mvp.common.views import IndexView, DashboardView
+from pet_mvp.common.views import AboutView, IndexView, DashboardView
 
 urlpatterns = [
     path('', login_not_required(IndexView.as_view()), name='index'),
+    path('about/', login_not_required(AboutView.as_view()), name='about'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
